07a0bd31d80b3a00b336f0deb492427b87c1332d
Imran Zukorlic <mr.silver020@gmail.com>
2022-07-26 14:51:26 +0200

    dom

---
+++ app.js
@@ -0,0 +1,64 @@
+// console.dir(document)
+// console.log(document.);
+// console.log(document.head);
+// console.log(document.all );
+
+//getElementById//
+// var headerTitle = document.getElementById('header-title')
+// var header = document.getElementById('main-header')
+// console.log(headerTitle);
+// headerTitle.textContent = "hello"
+// headerTitle.innerText= "goodbay"
+// headerTitle.innerHTML = "<h3>hello</h3>"
+// header.style.borderBottom = 'solid 3px #000'
+
+//getElementByClassName//
+// var items = document.getElementsByClassName('list-group-item')
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent = "Hello2"
+// items[1].style.backgroundColor = "yellow"
+
+// // items.style.backgroundColor = "blue"  = dosen't work
+// //we must like this
+// for (let i = 0; i < items.length; i++) {
+//     items[i].style.backgroundColor = 'blue';
+    
+// }
+
+//getEllementByTagName//
+
+//querSelector//
+
+var header = document.querySelector ( '#main-header' );
+header.style.borderBottom = ' solid 4px #ccc '
+
+var input = document.querySelector ( 'input' )
+input.value = ' Hello World '
+
+var submit = document.querySelector ( 'input[type="submit"]' )
+submit.value = "SEND"
+
+var item = document.querySelector ( '.list-group-item' )
+item.style.color = 'red'
+
+var lastItem = document.querySelector ( '.list-group-item:last-child' )
+lastItem.style.color = 'blue' 
+
+var secondItem = document.querySelector ( '.list-group-item:nth-child(2)' )
+secondItem.style.color = 'blue' 
+
+//querySelectorAll
+var title = document.querySelectorAll('.title')
+console.log(title);
+
+title[0].textContent ="Hello world"
+
+var odd = document.querySelectorAll('li:nth-child(odd)')//svaki parni
+var even = document.querySelectorAll('li:nth-child(even)')//svaki neparan
+for (let i = 0; i < odd.length; i++) {
+    odd[i].style.background = "#f4f4f4"
+    even[i].style.background = "#ccc"
+}
+
+
